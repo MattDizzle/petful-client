11eb7c206fd308f8f820bb509cea3ebbc78b5a59
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-28 11:09:06 -0700

    changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   // API_ENDPOINT: 'http://localhost:8000/api'
-  API_ENDPOINT: 'https://immense-thicket-82845.herokuapp.com/api'
-}
+  API_ENDPOINT: "https://immense-thicket-82845.herokuapp.com/api",
+};
